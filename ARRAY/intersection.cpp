059a75e595691e9bce222580d245266af5ff7eec
Kartikey-051107 <Kartikey05gupta@gmail.com>
2026-03-19 03:03:01 +0530

    .

---
+++ ARRAY/intersection.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main() {
+    long t;
+    cin>>t;
+    while(t--){
+        int m,n;
+        cin>>m;
+        int arr1[m];
+        
+        for (int i=0;i<m;i++){
+            cin>>arr1[i];
+        }
+        cin>>n;
+
+        int arr2[n];
+        
+        for (int i=0;i<n;i++){
+            cin>>arr2[i];
+        }
+        int printed=0;
+
+        for (int i=0;i<m;i++){
+           int  k=arr1[i];
+           for(int j=0;j<n;j++){
+            if(k==arr2[i]){
+                if(printed>0){
+                    cout<<" ";
+                }
+                cout<<k;
+            }
+           }
+        }
+cout<<endl;
+
+    }
+}
