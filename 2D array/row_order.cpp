cbfbdb0c41fdad129ad4eee24b6de6d9dcdff25f
Kartikey-051107 <Kartikey05gupta@gmail.com>
2026-03-26 02:51:25 +0530

    .

---
+++ 2D array/row_order.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n;
+    cin>>m;
+    int arr[n][m];
+    for (int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+
+        }
+    }
+    int count=0;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j];
+           if(i<n-1 || j<m-1){
+            cout<<" ";
+           }
+            
+        }
+    }
+}
